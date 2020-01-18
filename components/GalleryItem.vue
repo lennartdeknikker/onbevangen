d1ad849fe672e9c17b7cae6a793ed562ba0f8dcc
lennartdeknikker <me@lenn4rt.com>
2020-01-18 14:57:27 +0100

    :sparkles: create image gallery component

---
+++ components/GalleryItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div :style="style" class="gallery-item"></div>
+</template>
+
+<script>
+export default {
+  props: {
+    woman: {
+      type: Array,
+      required: true
+    }
+  },
+  computed: {
+    style() {
+      return `background: center / cover no-repeat url(${this.woman.image});`
+    }
+  }
+}
+</script>
+
+<style scoped>
+.gallery-item {
+  height: 100%;
+  width: 33%;
+}
+</style>
