fa86045fbedd37a2915b3082bf92d5b248d668bb
lennartdeknikker <me@lenn4rt.com>
2020-01-19 15:10:32 +0100

    :wrench: change cms config to add social links

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="gallery-container">
+    <button @click="imageIndex -= 1" class="slide-button-left slide-button">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 110.18 252.62"
+        class="arrow-svg"
+      >
+        <defs>
+          <style>
+            .a {
+              fill: none;
+              stroke: white;
+              stroke-linecap: round;
+              stroke-miterlimit: 10;
+              stroke-width: 8px;
+            }
+          </style>
+        </defs>
+        <title>arrow</title>
+        <polyline class="a" points="106.18 4 5.18 126.5 105.87 248.62" />
+      </svg>
+    </button>
     <GalleryItem
       v-for="woman of women"
       :key="woman.full_name + '_image_container'"
@@ -7,6 +28,27 @@
       :data-person="woman.full_name"
     >
     </GalleryItem>
+    <button @click="imageIndex += 1" class="slide-button-right slide-button">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 110.18 252.62"
+        class="arrow-svg"
+      >
+        <defs>
+          <style>
+            .a {
+              fill: none;
+              stroke: white;
+              stroke-linecap: round;
+              stroke-miterlimit: 10;
+              stroke-width: 8px;
+            }
+          </style>
+        </defs>
+        <title>arrow-right</title>
+        <polyline class="a" points="4 4 105 126.5 4.31 248.62" />
+      </svg>
+    </button>
   </div>
 </template>
 
@@ -16,6 +58,11 @@ import GalleryItem from './GalleryItem.vue'
 export default {
   components: {
     GalleryItem
+  },
+  data() {
+    return {
+      imageIndex: 0
+    }
   },
   computed: {
     women() {
@@ -30,5 +77,40 @@ export default {
   width: 100%;
   height: 100vh;
   display: flex;
+  overflow: hidden;
+  flex-wrap: wrap;
+}
+
+.slide-button {
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 5rem;
+  background-color: transparent;
+  border: 0;
+  color: white;
+  font-weight: bold;
+  font-size: 3em;
+  overflow: hidden;
+  cursor: pointer;
+}
+
+.slide-button:active {
+  outline: none;
+  border: none;
+}
+
+.slide-button-left {
+  left: 0;
+}
+
+.slide-button-right {
+  right: 0;
+}
+
+.arrow-svg {
+  width: 0.8em;
 }
 </style>
---
+++ components/Navigation.vue
@@ -0,0 +1,57 @@
+<template>
+  <nav class="nav-container">
+    <ul class="nav-items">
+      <li class="nav-item"><a href="/nieuws">nieuws</a></li>
+      <li class="nav-item"><a href="/nieuws">informatie</a></li>
+      <li><Social /></li>
+      <li class="nav-item">
+        <a href="/nieuws"
+          ><span class="language-indicator-not-selected">EN</span>/<span
+            >NL</span
+          ></a
+        >
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+import Social from './Social.vue'
+export default {
+  components: {
+    Social
+  }
+}
+</script>
+
+<style scoped>
+.nav-container {
+  display: flex;
+  width: 50%;
+}
+
+a {
+  color: white;
+  font-family: var(--font);
+  text-decoration: none;
+  font-size: 1.2em;
+}
+
+.nav-items {
+  display: flex;
+  justify-content: flex-end;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+  padding: 2rem;
+}
+
+.nav-item {
+  margin-left: 2rem;
+}
+
+.language-indicator-not-selected {
+  color: #b3b3b3;
+}
+</style>
