3cf30ac9f0aeaac625a865c95868e4804b4edc2e
lennartdeknikker <me@lenn4rt.com>
2020-02-18 17:44:34 +0100

    add news

--- components/News.vue
+++ components/News.vue
@@ -103,7 +103,7 @@
         <a
           v-for="item of newsItems"
           :key="item.date"
-          :href="'/nieuwsberichten/' + item.title"
+          :href="'/nieuwsberichten/' + item.title.replace(' ', '-')"
         >
           <li>
             <h3>
